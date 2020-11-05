944431fbcbf18f5c86c1346371cbe491981cdfd4
Dalembert Monzon <DDMU@users.noreply.github.com>
2020-11-05 02:08:24 -0500

    Change time jwt

--- otorongo_back_end/settings.py
+++ otorongo_back_end/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
 
 # REST_FRAMEWORK_EXPIRY_TIME = 12
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(hours=5),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=20),
     'ROTATE_REFRESH_TOKENS': False,
 }
